a6e2bf993cf1aa8ab0c587598e707cbaf46c4dc2
Nikita Parkhomenko <nikita.parkhom@gmail.com>
2021-02-22 18:21:16 +0200

    development
    - Create _app page to access global styles
    - Create _document file to extend basic HTML and add fonts link

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+
+import '../styles/globals.css';
+
+export default function MyApp({ Component, pageProps }) {
+    return (
+        <>
+            <Component {...pageProps} />
+        </>
+    )
+}
