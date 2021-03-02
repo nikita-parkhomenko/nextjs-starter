9b077f03d06f203127aeb5c4eeb27bf946ed10dd
nikita-parkhomenko <nikita.parkhom@gmail.com>
2021-03-02 18:27:41 +0200

    development
    - we must to use getStaticPaths in dynamic routes pages
    - generate params object with all id's of employees
    - use getStaticProps method to generate layout in build time
    - use params object with id of employee which we created in getStaticPaths method

--- pages/user.js
+++ pages/user.js
@@ -1,8 +1,17 @@
 
-import {Badge, Container } from 'reactstrap';
+import { Badge, Container } from 'reactstrap';
 import { PrivatLayout } from '../layout/privat-layout';
 
-export default function User(user) {
+export async function getStaticProps() {
+    const resp = await fetch('https://api.github.com/users/nikita-parkhomenko');
+    const user = await resp.json();
+
+    return {
+        props: { user }
+    };
+}
+
+export default function User ({ user }) {
     return (
         <PrivatLayout>
             <Container className="d-flex flex-column align-items-center" >
@@ -14,17 +23,10 @@ export default function User(user) {
                         alt="profile"
                     />
                 </div>
-                <h1>Hi, {user.name}</h1>
+                <h1>Hello, {user.name}</h1>
                 <Badge className="p-2 mb-2" pill color="info">{user.bio}</Badge>
                 <p>Company: {user.company}</p>
             </Container>
         </PrivatLayout>
     )
-}
-
-User.getInitialProps = async () => {
-    const resp = await fetch('https://api.github.com/users/nikita-parkhomenko');
-    const user = await resp.json();
-
-    return user;
 }
